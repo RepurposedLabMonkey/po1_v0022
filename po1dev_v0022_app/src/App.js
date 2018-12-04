9b76a123b49096e32432711e92864ad46f2d6e2f
RepurposedLabMonkey <RepurposedLabMonkey@users.noreply.github.com>
2018-12-04 15:41:58 -0800

    react dom router setup

--- po1dev_v0022_app/src/App.js
+++ po1dev_v0022_app/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 // import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <h2>React CRUD Tutorial</h2>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="container">
+				<h2>React CRUD Tutorial</h2>
+			</div>
+		);
+	}
 }
 
 export default App;
