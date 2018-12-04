dc55b8df8efc2bb41af806c1e7587b0303315857
RepurposedLabMonkey <RepurposedLabMonkey@users.noreply.github.com>
2018-12-04 15:57:37 -0800

    react components added

--- po1dev_v0022_app/src/index.js
+++ po1dev_v0022_app/src/index.js
@@ -8,8 +8,8 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 	<BrowserRouter>
-	<App />, document.getElementById('root')    
-	</BrowserRouter>
+	<App />
+	</BrowserRouter>, document.getElementById('root')    
 );
 
 // If you want your app to work offline and load faster, you can change
