456f4ad1e0a442669f9f69ca33fd83cce674dbbc
RepurposedLabMonkey <RepurposedLabMonkey@users.noreply.github.com>
2018-12-04 17:01:56 -0800

    create form added

--- po1dev_v0022_app/src/App.js
+++ po1dev_v0022_app/src/App.js
@@ -9,36 +9,36 @@ import Index from './components/index.component.js';
 
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to={'/'} className="navbar-brand">React CRUD Example</Link>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                  <Link to={'/'} className="nav-link">Home</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to={'/create'} className="nav-link">Create</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to={'/index'} className="nav-link">Index</Link>
-                </li>
-              </ul>
-            </div>
-          </nav> <br/>
-          <h2>Welcome to React CRUD Tutorial</h2> <br/>
-          <Switch>
-              <Route exact path='/create' component={ Create } />
-              <Route path='/edit/:id' component={ Edit } />
-              <Route path='/index' component={ Index } />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div className="container">
+					<nav className="navbar navbar-expand-lg navbar-light bg-light">
+						<Link to={'/'} className="navbar-brand">PO1 v0.0.2.2</Link>
+						<div className="collapse navbar-collapse" id="navbarSupportedContent">
+							<ul className="navbar-nav mr-auto">
+								<li className="nav-item">
+									<Link to={'/'} className="nav-link">Home</Link>
+								</li>
+								<li className="nav-item">
+									<Link to={'/create'} className="nav-link">Create</Link>
+								</li>
+								<li className="nav-item">
+									<Link to={'/index'} className="nav-link">Index</Link>
+								</li>
+							</ul>
+						</div>
+					</nav> <br/>
+					<h2>PO1 development build v0.0.2.2</h2> <br/>
+					<Switch>
+							<Route exact path='/create' component={ Create } />
+							<Route path='/edit/:id' component={ Edit } />
+							<Route path='/index' component={ Index } />
+					</Switch>
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
